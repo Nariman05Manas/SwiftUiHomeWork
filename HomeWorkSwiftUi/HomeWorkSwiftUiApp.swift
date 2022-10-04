baf3727b5a7ea1065ad0e5eaebcb12d8eb5f5049
Nariman05Manas <89251464474@mail.ru>
2022-10-04 17:51:31 +0300

    Initial Commit

---
+++ HomeWorkSwiftUi/HomeWorkSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWorkSwiftUiApp.swift
+//  HomeWorkSwiftUi
+//
+//  Created by qwerty on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWorkSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
