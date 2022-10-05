1a9868b9b1ef32a70473cca941104deb82d2a67f
Nariman05Manas <89251464474@mail.ru>
2022-10-05 14:36:21 +0300

    д/з SwiftUi готово

---
+++ HomeWorkSwiftUi/TabBar/FeedView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedView.swift
+//  HomeWorkSwiftUi
+//
+//  Created by qwerty on 05.10.2022.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text("Feed")
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
+
---
+++ HomeWorkSwiftUi/TabBar/PlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerView.swift
+//  HomeWorkSwiftUi
+//
+//  Created by qwerty on 05.10.2022.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        Text("Player")
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
